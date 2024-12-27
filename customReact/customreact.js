5fbfaeecacf20c652edcd192d11aa358f844682b
Adam0019 <aniruddhamallick2021@gmail.com>
2024-12-28 00:43:46 +0530

    custom react

---
+++ customReact/customreact.js
@@ -0,0 +1,26 @@
+function CustomReact(Reactelements, container) {
+  /*const domElement = document.createElement(Reactelements.type);
+  domElement.innerHTML = Reactelements.children;
+  domElement.setAttribute("href", Reactelements.props.href);
+  domElement.setAttribute("target", Reactelements.props.target);
+  container.appendChild(domElement);*/
+
+  const element = document.createElement(Reactelements.type);
+  element.innerHTML = Reactelements.children;
+  for (const prop in Reactelements.props) {
+    if (prop === "children") continue;
+    element.setAttribute(prop, Reactelements.props[prop]);
+  }
+  container.appendChild(element);
+}
+const Reactelements = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "click here to visit google",
+};
+const Mainroot = document.querySelector("#root");
+
+customRender(Reactelements, Mainroot);
