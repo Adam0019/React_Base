097d595a4562693931a2e4fe83cc8bfd67467141
Adam0019 <aniruddhamallick2021@gmail.com>
2024-12-28 18:44:18 +0530

    fixed2

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+//import App from "./App.jsx";
+const elementreact = (
+  <a href="https://google.com/" target="_blank">
+    click me!
+  </a>
 );
+
+createRoot(document.getElementById("root")).render(elementreact);
